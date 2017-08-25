726b86aca03c7571c39ab6041513133074c03b69
mmarszal7 <mati.m7@o2.pl>
2017-08-25 17:19:45 +0200

    Added escape mechanism

--- TrayTimer/ResourcesAndHelpers/Notification.xaml.cs
+++ TrayTimer/ResourcesAndHelpers/Notification.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
@@ -8,9 +9,9 @@ namespace TrayTimer
     public partial class Notification : Window
     {
         public int NotificationClicks = 0;
-        private void NotificationClick(object sender, System.Windows.Input.MouseButtonEventArgs e) { NotificationClicks = 1 ; }
         private int marginX = 150;
         private int marginY = 20;
+        private int numberOfEscapes = 0;
 
         private Random rnd = new Random();
         private SolidColorBrush[] ColorList = new SolidColorBrush[5]
@@ -25,17 +26,27 @@ namespace TrayTimer
         public Notification(string title)
         {
             InitializeComponent();
-
-            Dispatcher.BeginInvoke(DispatcherPriority.ApplicationIdle, new Action(() =>
-            {
-                var workingArea = System.Windows.SystemParameters.WorkArea;
-                
-                this.Left = rnd.Next(marginX, (int)workingArea.Width - marginX - (int)NotificationBody.Width);
-                this.Top = rnd.Next(marginY, (int)workingArea.Height - marginY - (int)NotificationBody.Height);
-            }));
-
             TextField.Text = title;
-            NotificationBackground.Background = ColorList[rnd.Next(0,4)];
+            DrawPosition();
+
+            numberOfEscapes = rnd.Next(0, 2) == 1 ? rnd.Next(1, 3) : 0; // 50% change for not escaping at all
         }
+
+        private void DrawPosition()
+        {
+            var workingArea = System.Windows.SystemParameters.WorkArea;
+            this.Left = rnd.Next(marginX, (int)workingArea.Width - marginX - (int)NotificationBody.Width);
+            this.Top = rnd.Next(marginY, (int)workingArea.Height - marginY - (int)NotificationBody.Height);
+
+            NotificationBackground.Background = ColorList[rnd.Next(0, 4)];
+        }
+
+        private void NotificationClick(object sender, System.Windows.Input.MouseButtonEventArgs e) { NotificationClicks = 1; }
+        private void NotificationFocus(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (numberOfEscapes > 0) { DrawPosition(); }
+            numberOfEscapes--;
+        }
+
     }
 }
