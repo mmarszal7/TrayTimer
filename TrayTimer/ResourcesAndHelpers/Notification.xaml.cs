36733d6f062699b8128e17cc294ca2e388f6da31
mmarszal7 <mati.m7@o2.pl>
2017-08-28 17:19:52 +0200

    Added escape indicator

--- TrayTimer/ResourcesAndHelpers/Notification.xaml.cs
+++ TrayTimer/ResourcesAndHelpers/Notification.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Windows;
 using System.Windows.Media;
-using System.Windows.Threading;
 
 namespace TrayTimer
 {
@@ -12,6 +10,7 @@ namespace TrayTimer
         private int marginX = 150;
         private int marginY = 20;
         private int numberOfEscapes = 0;
+        private string notificationTitle;
 
         private Random rnd = new Random();
         private SolidColorBrush[] ColorList = new SolidColorBrush[5]
@@ -26,10 +25,9 @@ namespace TrayTimer
         public Notification(string title)
         {
             InitializeComponent();
-            TextField.Text = title;
+            notificationTitle = title;
+            numberOfEscapes = rnd.Next(0, 3) == 0 ? rnd.Next(1, 3) : 0; // 50% change for not escaping at all
             DrawPosition();
-
-            numberOfEscapes = rnd.Next(0, 2) == 1 ? rnd.Next(1, 3) : 0; // 50% change for not escaping at all
         }
 
         private void DrawPosition()
@@ -37,15 +35,16 @@ namespace TrayTimer
             var workingArea = System.Windows.SystemParameters.WorkArea;
             this.Left = rnd.Next(marginX, (int)workingArea.Width - marginX - (int)NotificationBody.Width);
             this.Top = rnd.Next(marginY, (int)workingArea.Height - marginY - (int)NotificationBody.Height);
-
+            
+            TextField.Text = String.Format("{0} ({1})", notificationTitle, numberOfEscapes);
             NotificationBackground.Background = ColorList[rnd.Next(0, 4)];
         }
 
         private void NotificationClick(object sender, System.Windows.Input.MouseButtonEventArgs e) { NotificationClicks = 1; }
         private void NotificationFocus(object sender, System.Windows.Input.MouseEventArgs e)
         {
-            if (numberOfEscapes > 0) { DrawPosition(); }
             numberOfEscapes--;
+            if (numberOfEscapes >= 0) { DrawPosition(); }
         }
 
     }
